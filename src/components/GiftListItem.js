8a8cb0bf82468ec6900e2ba34c9158c71c4a2761
Diego Ferreyra <diego@404.pe>
2017-01-04 11:11:58 -0500

    Finished the work on the gift list item

--- src/components/GiftListItem.js
+++ src/components/GiftListItem.js
@@ -27,21 +27,49 @@ const GiftListItem = React.createClass({
       }
     }
   },
+  renderCommittedByUser() {
+    const message = (this.props.committedByUser === this.props.total) ? `You committed to these` : `You committed to ${this.props.committedByUser}`
+    const markup = <div className="self-committed-info col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <div className="self-committed rounded">
+        <span>{message}</span><br/>
+        <a href="#">Click here to change</a>
+      </div>
+    </div>
+    if ((this.props.committedByUser !== undefined) && (this.props.committedByUser > 0)) {
+      return markup
+    } else {
+      return null
+    }
+  },
+  renderIsCommitted() {
+    if ((this.props.total - this.props.committed) === 0) {
+      return "isCommitted"
+    } else {
+      if ((this.props.committedByUser !== undefined) && (this.props.committedByUser > 0)) {
+        return "isCommitted"
+      } else {
+        return null
+      }
+    }
+  },
   render () {
-    const locationInfo = this.renderLocationInfo();
-    const similarInfo = this.renderSimilarInfo();
-    const remainderInfo = this.renderRemainderInfo();
+    const locationInfo = this.renderLocationInfo()
+    const similarInfo = this.renderSimilarInfo()
+    const remainderInfo = this.renderRemainderInfo()
+    const isCommitted = this.renderIsCommitted()
     const remainder = ((this.props.total - this.props.committed) > 1) ? " " + (this.props.total - this.props.committed).toString() : null
     const giveLessBtn = ((this.props.total - this.props.committed) > 1) ? <a href="#" className="give-less-link">give less than {remainder}</a> : null
     const currency = Currencies[this.props.price.currency].symbol
+    const committedByUser = this.renderCommittedByUser()
     return (
-      <div className="u_gift-item col-xs-12 col-sm-6 col-md-4">
+      <div className={`u_gift-item col-xs-12 col-sm-6 col-md-4 ${isCommitted}`}>
         <div className="image-container rounded" style={{backgroundImage:'url(\'http://images.landofnod.com/is/image/LandOfNod/Crib_Anderson_Walnut_V1/$web_setitem$/130831065716/andersen-crib-american-walnut.jpg\')'}}>
           <img src="http://images.landofnod.com/is/image/LandOfNod/Crib_Anderson_Walnut_V1/$web_setitem$/130831065716/andersen-crib-american-walnut.jpg" alt="Product" />
         </div>
         <h3 className="title">{this.props.name}</h3>
         <hr/>
         <div className="item-content row">
+          {committedByUser}
           <div className="info col-xs-12 col-sm-12 col-md-12 col-lg">
             {locationInfo}{(locationInfo !== null)?<br/>:null}
             {similarInfo}{(similarInfo !== null)?<br/>:null}
